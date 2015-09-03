8d8abdb7af2208d6ed41b498017baac848e6cb5a
prashantkumar1 <prashant.1.kumar@gmail.com>
2015-09-03 08:28:03 +0530

    cropping of photo
    This app selects a picture by both camera and gallery and then allow the
    user to crop the picture.

---
+++ app/src/main/java/com/example/myultra/crop/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.myultra.crop;
+
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.preference.DialogPreference;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //keep track of gallery intent
+    final int GALLERY_USE=2;
+    //keep track of camera capture intent
+    final int CAMERA_CAPTURE = 1;
+    //captured picture uri
+    private Uri picUri;
+    //keep track of cropping intent
+    final int PIC_CROP =3;
+;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void open(View view) {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Select the option for image ");
+        alertDialogBuilder.setPositiveButton("CAMERA", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //use standard intent to capture an image
+                Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //we will handle the returned data in onActivityResult
+                startActivityForResult(captureIntent, CAMERA_CAPTURE);
+
+            }
+
+        });
+
+
+        alertDialogBuilder.setNegativeButton("GALARY", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+
+                //use standard intent to open gallery
+                Intent galleryintent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                //we will handle the returned data in onActivityResult
+                startActivityForResult(galleryintent, GALLERY_USE);
+
+            }
+        });
+
+        AlertDialog alertDialog=alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            //user is returning from capturing an image using the camera
+            if(requestCode == CAMERA_CAPTURE){
+                //get the Uri for the captured image
+                picUri = data.getData();
+                //carry out the crop operation
+                performCrop();
+            }
+            else if (requestCode==GALLERY_USE)
+            {
+                picUri = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(picUri, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
+
+                performCrop();
+
+            }
+            //user is returning from cropping the image
+            else if(requestCode == PIC_CROP){
+                //get the returned data
+                Bundle extras = data.getExtras();
+                //get the cropped bitmap
+                Bitmap thePic = extras.getParcelable("data");
+                //retrieve a reference to the ImageView
+                ImageView picView = (ImageView)findViewById(R.id.imageView);
+                //display the returned cropped image
+                picView.setImageBitmap(thePic);
+
+            }
+        }
+
+    }
+    private void performCrop()
+    {
+        try {
+            //call the standard crop action intent (the user device may not support it)
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            //indicate image type and Uri
+            cropIntent.setDataAndType(picUri, "image/*");
+            //set crop properties
+            cropIntent.putExtra("crop", "true");
+            //indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            //indicate output X and Y
+            cropIntent.putExtra("outputX", 256);
+            cropIntent.putExtra("outputY", 256);
+            //retrieve data on return
+            cropIntent.putExtra("return-data", true);
+            //start the activity - we handle returning in onActivityResult
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        catch(ActivityNotFoundException anfe){
+            //display an error message
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
